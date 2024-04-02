fc06e5736545de8a9995581a767d24213eef8806
EvgenyEf84 <mainbox1984@mail.ru>
2024-04-02 23:08:30 +0300

    Second

--- My project (12)/Assets/Scripts/mover.cs
+++ My project (12)/Assets/Scripts/mover.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class mover : MonoBehaviour
 {
     [SerializeField] private float _speed;
-
+    
     private void Update()
     {
         var direction = transform.forward.normalized;
         transform.Translate(direction * _speed, Space.World);
+        //var nextPosition = transform.position;
+        //nextPosition.z += 0.05f;
+        //transform.position = nextPosition;
     }
 }
